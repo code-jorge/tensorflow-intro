97f1fe23e88da778498b67b54607315097097a07
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2023-05-27 16:43:14 +0200

    Smol improvements

--- src/App.js
+++ src/App.js
@@ -1,65 +1,129 @@
 import { useEffect, useRef, useState } from 'react'
-import { detectObjects, drawBoxes, establishDimensions } from './utils';
+import { detectObjects, drawBoxes, establishDimensions, getLogs } from './utils';
 import * as cocoSsd from '@tensorflow-models/coco-ssd'
 import * as tf from "@tensorflow/tfjs";
 import css from './App.module.css'
-
 
 const REFRESH_TIME = 5000;
 
 const App = ()=> {
 
+  // Webcam capturing
   const [isVideoActive, setVideoActive] = useState(false)
+  const [videoStatus, setVideoStatus] = useState('')
+  const [aspectRatio, setAspectRatio] = useState(0)
+
+  // Tensor flow capturing
+  const [model, setModel] = useState(null)
+
+  // Object detection
+  const [isDetectionActive, setDetectionActive] = useState(false)
+  const [detectedObjects, setDetectedObjects] = useState(0)
+
+  const [expanded, setExpanded] = useState(false)
 
   const videoRef = useRef(null)
   const canvasRef = useRef(null)
 
+  const logs = getLogs({
+    isVideoActive,
+    videoStatus,
+    model,
+    isDetectionActive,
+    detectedObjects
+  })
+
   useEffect(()=> {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-      console.error('No video source available')
+      setVideoStatus('No video stream available in this device.')
+      setVideoActive(false)
       return
     }
     navigator.mediaDevices.getUserMedia({ video: true })
       .then(stream => {
         videoRef.current.srcObject = stream
+        setVideoStatus('Video stream available.')
         setVideoActive(true)
       })
-      .catch(err => {
-        console.error('Error on getting user video flow', err)
+      .catch(() => {
+        setVideoStatus('Denied access to the video stream.')
+        setVideoActive(false)
       })
   }, [])
 
   useEffect(()=> {
-    if (!isVideoActive) return
-    const runObjectDetection = async () => {
+    const loadModel = async () => {
       await tf.ready()
       const model = await cocoSsd.load()
-      console.log('Model loaded successfully.')
-
-      const video = videoRef.current
-      const canvas = canvasRef.current
-
-      establishDimensions({ video, canvas })
-
-      setInterval(()=> {
-        detectObjects({ model, video })
-          .then(predictions => {
-            console.log('Objects found: ', predictions.length)
-            drawBoxes({ predictions, canvas })
-          });
-      }, REFRESH_TIME)
+      setModel(model)
     }
-    runObjectDetection();
-  }, [isVideoActive])
+    loadModel()
+  }, [])
+
+  useEffect(()=> {
+    if (!isVideoActive || !model) return
+    const video = videoRef.current
+    const canvas = canvasRef.current
+    const ratio = establishDimensions({ video, canvas })
+    if (ratio) setAspectRatio(ratio)
+  }, [isVideoActive, model])
+
+  useEffect(()=> {
+    if (!isVideoActive || !model || !aspectRatio) return
+    const video = videoRef.current
+    const canvas = canvasRef.current
+    setDetectionActive(true)
+    const detect = ()=> {
+      detectObjects({ model, video })
+        .then(predictions => {
+          setDetectedObjects(predictions.length)
+          drawBoxes({ predictions, canvas, aspectRatio })
+        });
+    }
+    setInterval(detect, REFRESH_TIME)
+    return ()=> {
+      clearInterval(detect)
+    }
+  }, [isVideoActive, model, aspectRatio])
 
 
   return (
-    <main className={css.main}>
-      <div className={css.content}>
-        <video className={css.video} ref={videoRef} autoPlay muted />
-        <canvas className={css.canvas} ref={canvasRef} />
-      </div>
-    </main>
+    <>
+      <header className={css.header}>
+        <h1 data-font="code" className={css.title}>Tensor Flow Object Detection</h1>
+      </header>
+      <main className={css.main}>
+        <div className={css.content}>
+          <video className={css.video} ref={videoRef} autoPlay muted />
+          <canvas className={css.canvas} ref={canvasRef} />
+        </div>
+      </main>
+      <footer className={css.footer}>
+        <div className={css.footerInfo}>
+          <h2 data-font="code" className={css.footerTitle}>
+            <span className={css.footerStatus}>
+              {logs.map((log, index)=> (
+                <span className={css.footerStatusItem} key={index}>{log.status}</span>
+              ))}
+            </span>
+            Logs
+          </h2>
+          <button className={css.logsButton} type="button" onClick={()=> setExpanded(e=> !e)}>
+            {expanded ? '\u25BC' : '\u25B2'}
+          </button>
+        </div>
+        {expanded &&  (
+          <div className={css.logs}>
+            {logs.map((log, index) => (
+              <div key={index} className={css.log}>
+                <span className={css.logStatus}>{log.status}</span>
+                <span data-font="code" className={css.message}>{log.message}</span>
+              </div>
+            ))}
+          </div>
+        )}
+      </footer>
+    </>
   )
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,22 @@
 const BOX_COLOR = '#00FFFF'
 const TEXT_COLOR = '#000000'
 
+const STATUS = [ '🔴', '🟠', '🟡', '🟢' ]
+
 export const establishDimensions = ({ video, canvas })=> {
-  const videoWidth = video.videoWidth
-  const videoHeight = video.videoHeight
+  // Determine the max video size regarding clientWidth and clientHeight
+  const horizontalRatio = video.clientWidth / video.videoWidth;
+  const verticalRatio = video.clientHeight / video.videoHeight;
+  const ratio = Math.max(horizontalRatio, verticalRatio);
+  // Apply the ratio to the video size
+  const videoWidth = video.videoWidth * ratio;
+  const videoHeight = video.videoHeight * ratio;
+  // Update the dimensions on the DOM
   video.width = videoWidth
   video.height = videoHeight
   canvas.width = videoWidth
   canvas.height = videoHeight
+  return ratio
 }
 
 export const detectObjects = async ({ model, video }) => {
@@ -16,7 +25,7 @@ export const detectObjects = async ({ model, video }) => {
   return predictions
 }
 
-export const drawBoxes = ({ predictions, canvas }) => {
+export const drawBoxes = ({ predictions, canvas, aspectRatio }) => {
   const ctx = canvas.getContext('2d')
   // Reset the canvas from previous shit
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
@@ -25,7 +34,8 @@ export const drawBoxes = ({ predictions, canvas }) => {
   ctx.textBaseline = 'top'
   // Draw each prediction
   predictions.forEach(prediction => {
-    const [x, y, width, height] = prediction.bbox
+    const baseCoordinates = prediction.bbox.slice(0, 4)
+    const [x, y, width, height] = baseCoordinates.map(c=> c*aspectRatio)
     // Draw a rectangle around the prediction area
     ctx.strokeStyle = BOX_COLOR
     ctx.lineWidth = 2
@@ -39,4 +49,20 @@ export const drawBoxes = ({ predictions, canvas }) => {
     ctx.fillStyle = TEXT_COLOR
     ctx.fillText(prediction.class, x+2, y+1)
   })
+}
+
+export const getLogs = ({ isVideoActive, videoStatus, model, isDetectionActive, detectedObjects })=> {
+  const logs = []
+  // Video log
+  if (isVideoActive) logs.push({ status: STATUS[3], message: videoStatus })
+  else if (videoStatus) logs.push({ status: STATUS[0], message: videoStatus })
+  else logs.push({ status: STATUS[1], message: 'Requesting video stream access...' })
+  // Model log
+  if (model) logs.push({ status: STATUS[3], message: 'Model loaded successfully.' })
+  else logs.push({ status: STATUS[1], message: 'Loading model...' })
+  // Detection log
+  if (!isDetectionActive) logs.push({ status: STATUS[1], message: 'Starting detection...' })
+  else logs.push({ status: STATUS[3], message: `Detected ${detectedObjects} objects.` })
+  // All done
+  return logs
 }
